b59ea7952af46a59b8f9b23a8996baaefcd44fa1
Corinna28 <lunacleju@gmail.com>
2023-04-19 14:37:31 +0200

    semaine16

--- Fichier _SQL/PAPYRUS/payrus.sql
+++ Fichier _SQL/PAPYRUS/payrus.sql
@@ -3,26 +3,30 @@
 
 -- Les besoins d'affichage
 
---     Quelles sont les commandes du fournisseur n°9120 ?
+--     1.Quelles sont les commandes du fournisseur n°9120 ?
 
 SELECT fournis.numfou,numcom
 FROM entcom
 JOIN fournis ON fournis.numfou=entcom.numfou
 WHERE fournis.numfou='9120';
 
---     Afficher le code des fournisseurs pour lesquels des commandes ont été passées.
+-- La commande WHERE dans une requête SQL permet d’extraire les lignes d’une base de données qui respectent une condition. Cela permet d’obtenir uniquement les informations désirées.
+
+--     2.Afficher le code des fournisseurs pour lesquels des commandes ont été passées.
 
 SELECT DISTINCT fournis.numfou
 FROM fournis 
 JOIN entcom ON fournis.numfou=entcom.numfou;
 
---     Afficher le nombre de commandes fournisseurs passées, et le nombre de fournisseur concernés.
+-- L’utilisation de la commande SELECT en SQL permet de lire toutes les données d’une ou plusieurs colonnes. Cette commande peut potentiellement afficher des lignes en doubles. Pour éviter des redondances dans les résultats il faut simplement ajouter DISTINCT après le mot SELECT.
+
+--     3.Afficher le nombre de commandes fournisseurs passées, et le nombre de fournisseur concernés.
 
 SELECT count(numcom),count(distinct fournis.numfou)
 FROM fournis
 JOIN entcom ON fournis.numfou=entcom.numfou;
 
---     Extraire les produits ayant un stock inférieur ou égal au stock d'alerte, et dont la quantité annuelle est inférieure à 1000.
+--     4.Extraire les produits ayant un stock inférieur ou égal au stock d'alerte, et dont la quantité annuelle est inférieure à 1000.
 
 --         Informations à fournir : n° produit, libellé produit, stock actuel, stock d'alerte, quantité annuelle)
 
@@ -30,40 +34,76 @@ SELECT codart, libart,stkale, stkphy,qteann
 FROM produit
 WHERE stkphy<=stkale AND qteann<1000;
 
---     Quels sont les fournisseurs situés dans les départements 75, 78, 92, 77 ?
+--     5.Quels sont les fournisseurs situés dans les départements 75, 78, 92, 77 ?
 --         L’affichage (département, nom fournisseur) sera effectué par département décroissant, puis par ordre alphabétique.
 SELECT nomfou,posfou
 FROM fournis
 WHERE posfou LIKE'75%' OR posfou LIKE '78%' OR posfou LIKE'92%' OR posfou LIKE'77%';
 
+-- L’opérateur LIKE est utilisé dans la clause WHERE des requêtes SQL. Ce mot-clé permet d’effectuer une recherche sur un modèle particulier. Il est par exemple possible de rechercher les enregistrements dont la valeur d’une colonne commence par telle ou telle lettre. Les modèles de recherches sont multiple.
+
 -- '%'extraira tous les noms commençant par '75'
 
 ------------------------------------------------------------------------------------------------------------------------------------
 
---     Quelles sont les commandes passées en mars et en avril ?
+--     6.Quelles sont les commandes passées en mars et en avril ?
 
-SELECT derliv
+-- La fonction DATE() renvoie uniquement la partie date des éléments présents dans la colonne spécifiée. Sa syntaxe est la suivante :
+-- SELECT DATE (derliv)
+-- FROM ligcom
+-- ;
+
+-- la fonction MONTH() permet d’extraire le numéro de mois à partir d’une date au format AAAA-MM-JJ. Si la date d’entrée est par exemple ‘2014-02-03’, la valeur de retour sera ‘3’. Cela permet d’associer le numéro de mois au nom du mois (3 = Mars).
+
+SELECT MONTH (derliv),numcom
 FROM ligcom
+;
 
 
 
---     Quelles sont les commandes du jour qui ont des observations particulières ?
+
+
+
+
+--     7.Quelles sont les commandes du jour qui ont des observations particulières ?
 
 --         Afficher numéro de commande et date de commande
 
---     Lister le total de chaque commande par total décroissant.
+SELECT numcom,obscom
+FROM entcom
+
+;
+--    8. Lister le total de chaque commande par total décroissant.
 
 --         Afficher numéro de commande et total
 
---     Lister les commandes dont le total est supérieur à 10000€ ; on exclura dans le calcul du total les articles commandés en quantité supérieure ou égale à 1000.
+SELECT numcom,qtecde
+FROM ligcom
+ORDER BY numcom DESC;
+
+-- La commande ORDER BY permet de trier les lignes dans un résultat d’une requête SQL. Il est possible de trier les données sur une ou plusieurs colonnes, par ordre ascendant ou descendant.
+
+
+
+--     9.Lister les commandes dont le total est supérieur à 10000€ ; on exclura dans le calcul du total les articles commandés en quantité supérieure ou égale à 1000.
 
 --         Afficher numéro de commande et total
 
---     Lister les commandes par nom de fournisseur.
+SELECT numcom,qtecde
+FROM ligcom
+WHERE qtecde>= 10000;
+
+
+--    10.Lister les commandes par nom de fournisseur.
 
 --         Afficher nom du fournisseur, numéro de commande et date
 
---     Sortir les produits des commandes ayant le mot "urgent' en observation.
+SELECT distinct fournis.nomfou,entcom.numcom
+FROM fournis
+JOIN entcom ON fournis.numfou=entcom.numfou
+JOIN ligcom ON entcom.numcom=ligcom.numcom;
+
+--     11.Sortir les produits des commandes ayant le mot "urgent' en observation.
 
 --         Afficher numéro de commande, nom du fournisseur, libellé du produit et sous total (= quantité commandée * prix unitaire)
 
