e3af9e13d4ff4b87d5d7b7e59f76ea92ec2789ed
Corinna28 <lunacleju@gmail.com>
2023-04-21 11:51:25 +0200

    Travail Sem 16

--- Fichier _SQL/PAPYRUS/payrus.sql
+++ Fichier _SQL/PAPYRUS/payrus.sql
@@ -161,9 +161,13 @@ WHERE prix1 < (SELECT MIN( prix1 ) AS mini
 FROM vente
 WHERE codart LIKE 'R%')
 
---     Sortir la liste des fournisseurs susceptibles de livrer les produits dont le stock est inférieur ou égal à 150 % du stock d'alerte.
+--    15. Sortir la liste des fournisseurs susceptibles de livrer les produits dont le stock est inférieur ou égal à 150 % du stock d'alerte.
 
 --         La liste sera triée par produit puis fournisseur
+
+
+
+
 
 --     Sortir la liste des fournisseurs susceptibles de livrer les produits dont le stock est inférieur ou égal à 150 % du stock d'alerte, et un délai de livraison d'au maximum 30 jours.
 
